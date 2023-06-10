90ff2a2821d35705097e1c4ff524d44c43717be6
Botir <t19a003b@jdu.uz>
2023-06-10 12:02:15 +0500

    Save Token

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
-const getArgs = require('./helpers/args')
+import getArgs from './helpers/args.js'
+import { printError, printSuccess, printHelp } from './services/log.service.js'
+import { saveKeyValue, getKeyValue } from './services/storage.service.js';
+
+const saveToken = async token =>{
+    try{
+        await saveKeyValue('token', token)
+        printSuccess('Token was saved')
+    }catch(error){
+        printError(error.message)
+    }
+}
 
 const startCLI = () =>{
     const args = getArgs(process.argv)
     console.log(args);
-
     if(args.h){
         // help
+        printHelp()
     }
     if(args.s){
         // save
     }
     if(args.t){
         // token
+        return saveToken(args.t)
     }
     // result
 }
